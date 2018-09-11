6aa33449739e27b4e2f4be5e23ea23937b4d883d
Pijoo <bartosz.surzyn@gmail.com>
2018-09-11 18:32:20 +0200

    numbers with commas

--- script/script.js
+++ script/script.js
@@ -13,6 +13,11 @@ function findMatches(wordToMatch, cities) {
   });
 }
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+
 function displayMatches() {
   const matchArray = findMatches(this.value, cities);
   const html = matchArray.map(place => {
@@ -22,7 +27,7 @@ function displayMatches() {
     return `
       <li>
         <span class="name">${cityName}, ${stateName}</span>
-        <span class="population">${place.population}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
       </li>`;
   }).join(''); //żeby połączyć w jeden string
   suggestions.innerHTML = html;
